1ca17a7187d1b0c63821ea3d35a8bdaee0169ed0
berbb <tintinridge@gmail.com>
2016-05-20 06:47:16 -0500

    Fix Fiddle buff Name

--- Champion/FiddleSticks/Programs.cs
+++ Champion/FiddleSticks/Programs.cs
@@ -81,7 +81,7 @@ namespace Feedlesticks
 
         public static bool IsWActive
         {
-            get { return ObjectManager.Player.HasBuff("fiddlebuff"); }
+            get { return ObjectManager.Player.HasBuff("Drain"); }
         }
 
         /// <summary>
