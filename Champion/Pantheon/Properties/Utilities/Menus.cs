185f6418dcdccc7ad641b2f5dd9426a43899c137
berbb <tintinridge@gmail.com>
2016-06-02 00:09:59 -0500

    Pantheon Ported to SDK Version
    - ez port

--- Champion/Pantheon/Properties/Utilities/Menus.cs
+++ Champion/Pantheon/Properties/Utilities/Menus.cs
@@ -1,7 +1,7 @@
 using EloBuddy.SDK.Menu.Values;
-using ExorAIO.Utilities;
+using ExorSDK.Utilities;
 
-namespace ExorAIO.Champions.Pantheon
+namespace ExorSDK.Champions.Pantheon
 {
     /// <summary>
     ///     The menu class.
@@ -13,30 +13,45 @@ namespace ExorAIO.Champions.Pantheon
         /// </summary>
         public static void Initialize()
         {
-            Variables.QMenu = Variables.Menu.AddSubMenu("Use Q to:", "qmenu");
-            Variables.QMenu.Add("qspell.combo", new CheckBox("Combo"));
-            Variables.QMenu.Add("qspell.ks", new CheckBox("KillSteal"));
-            Variables.QMenu.Add("qspell.harass", new CheckBox("Harass"));
-            Variables.QMenu.Add("qspell.jgc", new CheckBox("JungleClear"));
-            Variables.QMenu.Add("qspell.mana", new Slider("Harass/JungleClear: Mana >= x", 50, 10, 99));
+            /// <summary>
+            ///     Sets the menu for the Q.
+            /// </summary>
+            Vars.QMenu = Vars.Menu.AddSubMenu("Use Q to:");
+            {
+                Vars.QMenu.Add("combo", new CheckBox("Combo"));
+                Vars.QMenu.Add("killsteal", new CheckBox("KillSteal"));
+                Vars.QMenu.Add("harass", new Slider("Harass / if Mana >= x%", 50, 10, 101));
+                Vars.QMenu.Add("jungleclear", new Slider("JungleClear / if Mana >= x%", 50, 10, 101));
+            }
 
-            Variables.WMenu = Variables.Menu.AddSubMenu("Use W to:", "wmenu");
-            Variables.WMenu.Add("wspell.combo", new CheckBox("Combo"));
-            Variables.WMenu.Add("wspell.ks", new CheckBox("KillSteal"));
-            Variables.WMenu.Add("wspell.ir", new CheckBox("Interrupt Enemy Channels"));
+            /// <summary>
+            ///     Sets the menu for the W.
+            /// </summary>
+            Vars.WMenu = Vars.Menu.AddSubMenu("Use W to:");
+            {
+                Vars.WMenu.Add("combo", new CheckBox("Combo"));
+                Vars.WMenu.Add("killsteal", new CheckBox("KillSteal"));
+                Vars.WMenu.Add("interrupter", new CheckBox("Interrupt Enemy Channels"));
+            }
 
-            Variables.EMenu = Variables.Menu.AddSubMenu("Use E to:", "emenu");
-            Variables.EMenu.Add("espell.combo", new CheckBox("Combo"));
-            Variables.EMenu.Add("espell.farm", new CheckBox("Clear"));
-            Variables.EMenu.Add("espell.mana", new Slider("Clear: Mana >= x", 50, 10, 99));
+            /// <summary>
+            ///     Sets the menu for the E.
+            /// </summary>
+            Vars.EMenu = Vars.Menu.AddSubMenu("Use E to:");
+            {
+                Vars.EMenu.Add("combo", new CheckBox("Combo"));
+                Vars.EMenu.Add("clear", new Slider("Clear / if Mana >= x%", 50, 10, 101));
+            }
 
-            Variables.DrawingsMenu = Variables.Menu.AddSubMenu("Drawings", "drawingsmenu");
-            Variables.DrawingsMenu.Add("drawings.q", new CheckBox("Q Range"));
-                //.SetValue(false).SetFontStyle(FontStyle.Regular, Color.Green);
-            Variables.DrawingsMenu.Add("drawings.w", new CheckBox("W Range"));
-                //.SetValue(false).SetFontStyle(FontStyle.Regular, Color.Purple);
-            Variables.DrawingsMenu.Add("drawings.e", new CheckBox("E Range"));
-                //.SetValue(false).SetFontStyle(FontStyle.Regular, Color.Cyan);
+            /// <summary>
+            ///     Sets the drawings menu.
+            /// </summary>
+            Vars.DrawingsMenu = Vars.Menu.AddSubMenu("Drawings");
+            {
+                Vars.DrawingsMenu.Add("q", new CheckBox("Q Range"));
+                Vars.DrawingsMenu.Add("w", new CheckBox("W Range"));
+                Vars.DrawingsMenu.Add("e", new CheckBox("E Range"));
+            }
         }
     }
 }
